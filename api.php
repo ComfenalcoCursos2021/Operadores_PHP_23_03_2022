4d9d4b0c227caf61652ff3a9017e9e0ebfbd5af1
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-23 20:20:22 -0500

    Operador arigmetico resta

--- api.php
+++ api.php
@@ -1,22 +1,23 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    header("Content-type: application/json");
+    header("Content-type: application/json;charset=utf-8");
     $obj = (object) [
         "server" => (string) $_SERVER["HTTP_HOST"]
     ];
     $num1 = (int) rand(1,50);
-    $num2 = (float) 5.332;
+    $num2 = (float) (mt_rand(1,50).".".mt_rand(100,999));
 
-    // $obj->suma = (int) ($num1 + $num2);
+
+    $obj->resta = (float) number_format(($num1 - $num2), 3);
 
     $obj->num2 = $num2;
 
-    $num2 += $num1;
-    settype($num2, "int");
+    $num2 -= $num1;
+    // settype($num2, "int");
 
 
     $obj->num1 = $num1;
-    $obj->suma = $num2;
+    // $obj->resta = (float) number_format($num2, 3);
     print_r(json_encode($obj, JSON_PRETTY_PRINT));
 
 
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // https://comfenalcocursos2022.000webhostapp.com/Operadores_php_23_03_2022/api.php
 let enviar = async function(){
-    let peticion = await fetch("api.php");
+    let peticion = await fetch("https://comfenalcocursos2022.000webhostapp.com/Operadores_php_23_03_2022/api.php");
     let json = await peticion.text();
     console.log(json);
+    
 }
 enviar();
