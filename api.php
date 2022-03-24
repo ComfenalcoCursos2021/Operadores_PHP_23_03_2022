84288173944f8ab10d4b2ee671e686677f154ce9
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-23 21:22:15 -0500

    Operador arigmetico divicion

--- api.php
+++ api.php
@@ -4,19 +4,19 @@
     $obj = (object) [
         "server" => (string) $_SERVER["HTTP_HOST"]
     ];
-    $num1 = (int) rand(1,50);
-    $num2 = (float) (mt_rand(1,50).".".mt_rand(100,999));
+    $num1 = (float) (mt_rand(1,50).".".mt_rand(100,999));
+    $num2 = (int) rand(1,50);
 
 
-    $obj->multiplicacion = (float) number_format(($num1 * $num2), 3, ".", "");
+    $obj->divicion = (float) number_format(($num1 / $num2), 3, ".", "");
 
     $obj->num2 = $num2;
-    // $num2 *= $num1;
+    // $num2 /= $num1;
     // settype($num2, "int");
 
 
     $obj->num1 = $num1;
-    // $obj->multiplicacion = (float) number_format($num2, 3, ".", "");
+    // $obj->divicion = (float) number_format($num2, 3, ".", "");
     print_r(json_encode($obj, JSON_PRETTY_PRINT));
 
 
