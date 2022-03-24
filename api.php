5b20476c98809ee0c4257b6c64882f4aee15276a
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-23 21:08:16 -0500

    Operador arigmetico multiplicacion

--- api.php
+++ api.php
@@ -8,16 +8,15 @@
     $num2 = (float) (mt_rand(1,50).".".mt_rand(100,999));
 
 
-    $obj->resta = (float) number_format(($num1 - $num2), 3);
+    $obj->multiplicacion = (float) number_format(($num1 * $num2), 3, ".", "");
 
     $obj->num2 = $num2;
-
-    $num2 -= $num1;
+    // $num2 *= $num1;
     // settype($num2, "int");
 
 
     $obj->num1 = $num1;
-    // $obj->resta = (float) number_format($num2, 3);
+    // $obj->multiplicacion = (float) number_format($num2, 3, ".", "");
     print_r(json_encode($obj, JSON_PRETTY_PRINT));
 
 
